1b2e771013c8f39a6a913feec8367db537e1db86
MiguelMatMar <miguelmm0005@gmail.com>
2025-10-31 22:19:28 +0000

    He borrado todo lo anterior ya que no use javascript porque no sabia

--- Tema 3/Proyecto P3-1/index.php
+++ Tema 3/Proyecto P3-1/index.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="./#" method="post">
+    <form action="./php.php" method="post">
         <fieldset>
 
             <legend>Informacion Personal</legend>
@@ -16,28 +16,42 @@
             <label for="apellidoUsuario">Apellidos: <input type="text" name="apellidoUsuario" id="apellidoUsuario" value=<?php echo !empty($_POST["apellidoUsuario"]) ? $_POST["apellidoUsuario"] : ""?>></label><br>
             <label for="direccionUsuario">Direccion de Usuario: <input type="text" name="direccionUsuario" id="direccionUsuario" value=<?php echo !empty($_POST["direccionUsuario"]) ? $_POST["direccionUsuario"] : ""?>></label><br>
 
-            <label for="calleUsuario">Selecciona tu Calle: 
-            <?php // Apartado para seleccionar calle o avenida
-                if(!empty($_POST["selecciondeCalle"])){ // Si el usuario ha seleccionado alguna, se carga la pagina php.php para coger la funcion
-                    
-                    mostrarCalleAvenida(); // Muestra
-                }else{ // Sino vuelve a mostrar por defecto la seleccion de avenida
-                    echo 
-                    '
-                        <select name="seleccionCalleAvenida" id="seleccionCalleAvenida">
-                            <option value="calle">Calle</option>
-                            <option value="avenida">Avenida</option>
-                        </select>
-                    ';
-                    echo '<input type="submit" name="selecciondeCalle" value="selecciondeCalle">';
-                }
-            ?>
+            <label for="eleccionCalleOAvenida">Selecciona tu Calle/Avenida: 
+                <select name="eleccionCalleOAvenida" id="eleccionCalleOAvenida" onchange="updateAddressOptions()">
+                    <option value="" disabled selected>-- Seleccione una opción --</option>
+                    <option value="calle" <?php echo (isset($_POST["eleccionCalleOAvenida"]) && $_POST["eleccionCalleOAvenida"] == "calle") ? "selected" : ""; ?>>Calle</option>
+                    <option value="avenida" <?php echo (isset($_POST["eleccionCalleOAvenida"]) && $_POST["eleccionCalleOAvenida"] == "avenida") ? "selected" : ""; ?>>Avenida</option>
+                </select>
+            </label>
+            <br>
+            <div id="opcionesDinamicas" style="display: none;">
+                <label for="tipoViaSecundario">Elige el Tipo de Vía específico: 
+                    <select name="tipoViaSecundario" id="tipoViaSecundario">
+                        <option value="principal">Principal</option>
+                        <option value="secundaria">Secundaria</option>
+                        <option value="peatonal">Peatonal</option>
+                    </select>
+                </label>
+            </div>
+            
             </label>
         </fieldset>
         
     </form>
-    <form action="./php.php" method="post">
-        <input type="submit" value="Enviar Datos">
-    </form>
+   <script>
+        function updateAddressOptions() {
+            const selectPrincipal = document.getElementById('eleccionCalleOAvenida');
+            const opcionesDinamicas = document.getElementById('opcionesDinamicas');
+            const valorSeleccionado = selectPrincipal.value;
+
+            opcionesDinamicas.style.display = 'none';
+            if (valorSeleccionado === 'calle' || valorSeleccionado === 'avenida') {
+                opcionesDinamicas.style.display = 'block';
+            }
+        }
+
+        window.onload = updateAddressOptions;
+    </script>
+
 </body>
 </html>
