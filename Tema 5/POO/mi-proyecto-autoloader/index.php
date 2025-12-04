df73f199e4d514e24b138da8399b4b6c6a2b7393
MiguelMatMar <miguelmm0005@gmail.com>
2025-12-04 11:01:37 +0100

    Implementacion de una pequeña tabla

--- Tema 5/POO/mi-proyecto-autoloader/index.php
+++ Tema 5/POO/mi-proyecto-autoloader/index.php
@@ -6,6 +6,7 @@ require_once 'autoloader.php';
 // Importamos la clase usando su namespace completo para poder referirnos a ella
 // simplemente como 'Calculadora' en el código.
 use App\Utils\Calculadora;
+use App\Views\TablaGenerator;
 
 echo "<h1>Proyecto de Autoloading y Namespaces en PHP</h1>";
 echo "<hr>";
@@ -16,6 +17,9 @@ $calc = new Calculadora();
 $numeroA = 15;
 $numeroB = 7;
 
+// 2. Usamos las tablas
+$tabla1 = new TablaGenerator();
+
 echo "<h2>1. Prueba de Namespace y Autoloading</h2>";
 echo "<p>Clase instanciada: <strong>" . $calc->getClase() . "</strong></p>";
 
@@ -25,6 +29,9 @@ $resultado = $calc->sumar($numeroA, $numeroB);
 echo "<h2>2. Ejecución del Método</h2>";
 echo "<p>Operación: {$numeroA} + {$numeroB}</p>";
 echo "<p>Resultado: <strong>{$resultado}</strong></p>";
+
+echo "<h1> Generamos una tabla </h1>";
+echo $tabla1->generateTable();
 
 echo "<hr>";
 echo "<p>¡El autoloader y el namespace están funcionando correctamente!</p>";
