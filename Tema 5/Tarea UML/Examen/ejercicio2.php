68952dd3253028d87c1a550fde85a7edbc25eb6c
MiguelMatMar <miguelmm0005@gmail.com>
2025-12-10 13:25:17 +0100

    Ejercicio 2 - Examen - Terminado

--- Tema 5/Tarea UML/Examen/ejercicio2.php
+++ Tema 5/Tarea UML/Examen/ejercicio2.php
@@ -1,10 +1,88 @@
 <?php
 
     // Apartado 1
-    // Apartado 2
     // Apartado 3
+        interface Procesable{
+            public function depositar($monto):void;
+            public function retirar($monto):void;
+        }
+    // Apartado 2
+        // 2.1 
+            abstract class CuentaBase implements Procesable{
+                public function __construct(
+                    private string $iban,
+                    private string $titular,
+                    private float $saldo
+
+                ){}
+
+                abstract protected function aplicarMantenimiento():void;
+
+                public function retirar($monto):void{
+                    if($this -> saldo < $monto ){
+
+                        throw new Exception("No puedes retirar tanta cantidad, saldo minimo <br>");
+                    }
+                    $this -> saldo -= $monto;
+                }
+                public function depositar($monto):void{
+                    $this -> saldo += $monto;
+                }
+                public function getSaldo(){
+                    return $this -> saldo;
+                }
+            }
+        // 2.2
+            class CuentaAhorro extends CuentaBase{
+                public function __construct(
+                    private string $iban,
+                    private string $titular,
+                    private float $saldo,
+                    
+                ) {
+                    parent::__construct($iban, $titular, $saldo);
+                }
+                protected function aplicarMantenimiento():void{
+                    $this -> saldo *= 1.02;
+                }
+            }
+            class CuentaCorriente extends CuentaBase{
+                public function __construct(
+                    private string $iban,
+                    private string $titular,
+                    private float $saldo,
+                    
+                ) {
+                    parent::__construct($iban, $titular, $saldo);
+                }
+                protected function aplicarMantenimiento():void{
+                    $this -> saldo -= 10;
+                }
+            }
+    
     // Apartado 4
     
 
+    function realizarTransferencia($origen,$destino,$monto) {
+        try{
+            $origen -> retirar($monto);
+            $destino -> depositar($monto);
+            echo "<br> Transferencia realizada con exito <br>";
+        }catch(Exception $e){
+            echo "Problema: ".$e -> getMessage();
+        }
+    }
 
+?>
+<?php
+
+    $cuenta1 = new cuentaCorriente('iban1','Jose',5000);
+    $cuenta2 = new cuentaAhorro('iban2','Jose',3000);
+
+
+    echo "Cuenta 1 ". $cuenta1 -> getSaldo() . "<br>";
+    echo "Cuenta 2 ". $cuenta2 -> getSaldo() . "<br>";
+    
+    echo realizarTransferencia($cuenta1,$cuenta2,7100);
+    
 ?>
